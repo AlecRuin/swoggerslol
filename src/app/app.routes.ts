996fe238aef89634a5894bf0f519b058bb56a4c4
AlecRuin <ruinhard@gmail.com>
2024-09-10 14:22:50 -0500

    added a basic demo route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { RouterModule, Routes } from "@angular/router";
+import { AboutComponent } from "./about/about.component";
+import { NgModule } from "@angular/core";
+
+// import { AboutComponent } from './about/about.component';
+
+// export const routes: Routes = [
+//     {path:"about",component:AboutComponent}
+// ];
+
+const routes:Routes=[
+    {path:'about',component:AboutComponent}
+];
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+export class AppRoutingModule{}
