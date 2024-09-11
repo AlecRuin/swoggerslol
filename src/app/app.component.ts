a0a1f330a9b1444d4a6f2dda01a79a205ac0de16
AlecRuin <ruinhard@gmail.com>
2024-09-10 19:21:09 -0500

    messing around with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { routes } from './app.routes';
+@Component({
+  selector:"app-user",
+  template:`
+    <div>bruh</div>
+  `,
+  standalone:true
+})
+export class UserComponent{
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,8 @@
 // import { Routes } from '@angular/router';
 
-import { RouterModule, Routes } from "@angular/router";
+import { Routes } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
-import { NgModule } from "@angular/core";
 
-// import { AboutComponent } from './about/about.component';
-
-// export const routes: Routes = [
-//     {path:"about",component:AboutComponent}
-// ];
-
-const routes:Routes=[
+export const routes:Routes=[
     {path:'about',component:AboutComponent}
 ];
-@NgModule({
-    imports:[RouterModule.forRoot(routes)],
-    exports:[RouterModule]
-})
-export class AppRoutingModule{}
